2ffc76bd23e7b9e4ab46aa089e8c44242a8ae463
Baran Kilic <ge95bid@tum.de>
2023-12-30 15:45:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.learningapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imagebutton = findViewById(R.id.buch);
+        imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
