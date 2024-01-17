671bf5b70292109b188729215ae25b1902bd08af
Baran Kilic <ge95bid@tum.de>
2024-01-17 12:45:36 +0100

    Added third view

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton imagebutton = findViewById(R.id.buch);
+        ImageButton imagebutton = findViewById(R.id.frag);
         imagebutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
